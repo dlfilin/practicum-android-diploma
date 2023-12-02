3ea920009389e21df16bfd1ccb88e542c4fd4ba1
Extro21 <vostrik21@gmail.com>
2023-12-02 15:32:53 +0300

    add Koin

--- app/src/main/java/ru/practicum/android/diploma/App.kt
+++ app/src/main/java/ru/practicum/android/diploma/App.kt
@@ -3,6 +3,11 @@ package ru.practicum.android.diploma
 import android.app.Application
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
+import ru.practicum.android.diploma.common.di.dataModule
+import ru.practicum.android.diploma.common.di.domainModule
+import ru.practicum.android.diploma.common.di.repositoryModule
+import ru.practicum.android.diploma.common.di.uiModule
+import ru.practicum.android.diploma.common.di.viewModelModule
 
 class App : Application() {
 
@@ -12,11 +17,11 @@ class App : Application() {
         startKoin {
             androidContext(this@App)
             modules(
-//                dataModule,
-//                repositoryModule,
-//                domainModule,
-//                viewModelModule,
-//                UiModule
+                dataModule,
+                repositoryModule,
+                domainModule,
+                viewModelModule,
+                uiModule
             )
         }
     }
