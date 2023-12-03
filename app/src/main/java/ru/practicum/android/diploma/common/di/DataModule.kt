2e40817f9312bdf50995ba0db511ede207a2ed35
Jaroslav-89 <jaroslav89@bk.ru>
2023-12-03 17:26:37 +0400

    Добавил инициализацию Retrofit в Di Data module

--- app/src/main/java/ru/practicum/android/diploma/common/di/DataModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/common/di/DataModule.kt
@@ -3,6 +3,11 @@ package ru.practicum.android.diploma.common.di
 import androidx.room.Room
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import ru.practicum.android.diploma.common.data.network.HhApiService
+import ru.practicum.android.diploma.common.data.network.NetworkClient
+import ru.practicum.android.diploma.common.data.network.RetrofitNetworkClient
 import ru.practicum.android.diploma.favorites.data.db.AppDataBase
 
 val dataModule = module {
@@ -11,4 +16,17 @@ val dataModule = module {
         Room.databaseBuilder(androidContext(), AppDataBase::class.java, "dataBase.db").build()
     }
 
+    single<HhApiService> {
+        Retrofit.Builder()
+            .baseUrl(RetrofitNetworkClient.HH_BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(HhApiService::class.java)
+    }
+
+    single<NetworkClient> {
+        RetrofitNetworkClient(
+            context = androidContext(), hhApiService = get()
+        )
+    }
 }
