b438d93d4004dd9cc82fff73599c66d032687a96
SvetlanaTimchenko <svetlana010785@yandex.ru>
2023-12-03 19:24:42 +0300

    Добавлены TextLayoutInput и EditText для поиска и фильтра с обработкой событий

--- app/src/main/java/ru/practicum/android/diploma/search/ui/SearchFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/ui/SearchFragment.kt
@@ -1,11 +1,15 @@
 package ru.practicum.android.diploma.search.ui
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.MotionEvent
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.FragmentSearchBinding
+
 
 class SearchFragment : Fragment(R.layout.fragment_search) {
 
@@ -25,6 +29,34 @@ class SearchFragment : Fragment(R.layout.fragment_search) {
         binding.gotoVacancyFragmentBtn.setOnClickListener {
             findNavController().navigate(R.id.action_searchFragment_to_vacancy_graph)
         }
+
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s.isNullOrEmpty()) {
+                    binding.searchEditText.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_search, 0)
+                }
+                else {
+                    binding.searchEditText.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_clear, 0)
+                    binding.searchEditText.setOnTouchListener {view, motionEvent ->
+                        if ((motionEvent.action == MotionEvent.ACTION_UP) &&
+                            (motionEvent.rawX >= (binding.searchEditText.right - binding.searchEditText.compoundDrawables[2].bounds.width() * 2))
+                        ) {
+                            binding.searchEditText.setText("")
+                        }
+                        view.performClick()
+                        false
+                    }
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+        }
+
+        simpleTextWatcher.let { binding.searchEditText.addTextChangedListener(it) }
     }
 
     override fun onDestroyView() {
