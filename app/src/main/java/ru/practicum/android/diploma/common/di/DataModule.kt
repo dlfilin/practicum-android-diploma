8bdcc836dae11d71f25d251f4813a74df9ccc7b8
SvetlanaTimchenko <svetlana010785@yandex.ru>
2023-12-04 13:47:12 +0300

    исправляем по detekt

--- app/src/main/java/ru/practicum/android/diploma/common/di/DataModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/common/di/DataModule.kt
@@ -1,10 +1,10 @@
 package ru.practicum.android.diploma.common.di
 
 import androidx.room.Room
-import org.koin.android.ext.koin.androidContext
-import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import org.koin.android.ext.koin.androidContext
+import org.koin.dsl.module
 import ru.practicum.android.diploma.common.data.network.HhApiService
 import ru.practicum.android.diploma.common.data.network.NetworkClient
 import ru.practicum.android.diploma.common.data.network.RetrofitNetworkClient
