8a329070ce96125c06054e57b85d309c8eb8a1a7
SvetlanaTimchenko <svetlana010785@yandex.ru>
2023-12-04 13:35:14 +0300

    исправляем по detekt

--- app/src/main/java/ru/practicum/android/diploma/search/ui/SearchFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/ui/SearchFragment.kt
@@ -10,7 +10,6 @@ import androidx.navigation.fragment.findNavController
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.FragmentSearchBinding
 
-
 class SearchFragment : Fragment(R.layout.fragment_search) {
 
     private var _binding: FragmentSearchBinding? = null
