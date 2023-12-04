72f32856fc3375b170ef78fa94372dea3768696d
SvetlanaTimchenko <svetlana010785@yandex.ru>
2023-12-04 13:52:51 +0300

    исправляем по detekt

--- app/src/main/java/ru/practicum/android/diploma/common/di/DataModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/common/di/DataModule.kt
@@ -1,14 +1,14 @@
 package ru.practicum.android.diploma.common.di
 
 import androidx.room.Room
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import ru.practicum.android.diploma.common.data.db.AppDataBase
 import ru.practicum.android.diploma.common.data.network.HhApiService
 import ru.practicum.android.diploma.common.data.network.NetworkClient
 import ru.practicum.android.diploma.common.data.network.RetrofitNetworkClient
-import ru.practicum.android.diploma.common.data.db.AppDataBase
 
 val dataModule = module {
 
