3e4d7ab81b533b205df458ebf9da71b7cce0ff49
Jaroslav-89 <jaroslav89@bk.ru>
2023-12-06 21:20:34 +0400

    task#65 - закоммитил пока обновление иконки избранное

--- app/src/main/java/ru/practicum/android/diploma/vacancy/presentation/VacancyViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/vacancy/presentation/VacancyViewModel.kt
@@ -3,21 +3,20 @@ package ru.practicum.android.diploma.vacancy.presentation
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import ru.practicum.android.diploma.vacancy.domain.models.Vacancy
 
 class VacancyViewModel : ViewModel() {
 
     private val _state = MutableLiveData(VacancyScreenState())
     val state: LiveData<VacancyScreenState> get() = _state
 
-    fun isVacancyFavorite() = state.value?.vacancy?.isFavorite ?: false
-
-    fun toggleFavorite() {
-        // пока просто для наглядности
-        val update = state.value?.vacancy?.let {
-            it.copy(isFavorite = !it.isFavorite)
-        } ?: Vacancy()
-        _state.postValue(VacancyScreenState(update))
-    }
+//    fun isVacancyFavorite() = state.value?.vacancy?.isFavorite ?: false
+//
+//    fun toggleFavorite() {
+//        // пока просто для наглядности
+//        val update = state.value?.vacancy?.let {
+//            it.copy(isFavorite = !it.isFavorite)
+//        } ?: VacancyDetail()
+//        _state.postValue(VacancyScreenState(update))
+//    }
 
 }
