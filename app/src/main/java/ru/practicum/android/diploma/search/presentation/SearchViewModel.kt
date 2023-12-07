57b25b66f894ce6aec909d956b203922eb440032
Jaroslav-89 <107784053+Jaroslav-89@users.noreply.github.com>
2023-12-07 23:35:55 +0400

    Update SearchViewModel.kt

--- app/src/main/java/ru/practicum/android/diploma/search/presentation/SearchViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/presentation/SearchViewModel.kt
@@ -57,7 +57,7 @@ class SearchViewModel(
                 }
             }
 
-            (foundVacancyData?.items.isNullOrEmpty()) -> {
+            foundVacancyData?.items.isNullOrEmpty() -> {
                 renderState(
                     SearchScreenState.Empty
                 )
