1418afc0a5918106aa64bf745f20a52d85785212
Jaroslav-89 <107784053+Jaroslav-89@users.noreply.github.com>
2023-12-07 23:33:54 +0400

    Update RepositoryModule.kt

--- app/src/main/java/ru/practicum/android/diploma/common/di/RepositoryModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/common/di/RepositoryModule.kt
@@ -1,9 +1,11 @@
 package ru.practicum.android.diploma.common.di
 
 import org.koin.dsl.module
-import ru.practicum.android.diploma.favorites.data .mapper.FavoriteMapper
-import ru.practicum.android.diploma.favorites.data .repository.FavoriteRepositoryImpl
+import ru.practicum.android.diploma.favorites.data.mapper.FavoriteMapper
+import ru.practicum.android.diploma.favorites.data.repository.FavoriteRepositoryImpl
 import ru.practicum.android.diploma.favorites.domain.repository.FavoriteRepository
+import ru.practicum.android.diploma.search.data.repository.SearchRepositoryImpl
+import ru.practicum.android.diploma.search.domain.api.SearchRepository
 
 val repositoryModule = module {
     single<SearchRepository> {
