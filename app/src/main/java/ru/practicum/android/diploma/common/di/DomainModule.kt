1810e9bffc6bc9ca5074c6769773e762fa0ecef0
Jaroslav-89 <107784053+Jaroslav-89@users.noreply.github.com>
2023-12-07 23:17:24 +0400

    Update DomainModule.kt

--- app/src/main/java/ru/practicum/android/diploma/common/di/DomainModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/common/di/DomainModule.kt
@@ -1,6 +1,8 @@
 package ru.practicum.android.diploma.common.di
 
 import org.koin.dsl.module
+import ru.practicum.android.diploma.favorites.domain.FavoriteInteractor
+import ru.practicum.android.diploma.favorites.domain.impl.FavoriteInteractorImp
 import ru.practicum.android.diploma.search.domain.api.SearchInteractor
 import ru.practicum.android.diploma.search.domain.impl.SearchInteractorImpl
 
