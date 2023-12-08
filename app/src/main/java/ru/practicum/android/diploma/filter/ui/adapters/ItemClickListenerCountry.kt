17a5ac49bd98b748170a7bcb9160cd2f3056a220
Extro21 <vostrik21@gmail.com>
2023-12-08 16:12:27 +0300

    Task#70 - add layout country and adapter

---
+++ app/src/main/java/ru/practicum/android/diploma/filter/ui/adapters/ItemClickListenerCountry.kt
@@ -0,0 +1,7 @@
+package ru.practicum.android.diploma.filter.ui.adapters
+
+import ru.practicum.android.diploma.filter.domain.models.Country
+
+fun interface ItemClickListenerCountry {
+    fun onItemListener(item : Country)
+}
