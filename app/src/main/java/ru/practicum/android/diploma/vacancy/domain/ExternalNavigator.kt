41c6a5d48ab49982c1a32efc1aa9663cfff3dc70
SvetlanaTimchenko <svetlana010785@yandex.ru>
2023-12-09 19:24:48 +0300

    функциональность звонка на номер

--- app/src/main/java/ru/practicum/android/diploma/vacancy/domain/ExternalNavigator.kt
+++ app/src/main/java/ru/practicum/android/diploma/vacancy/domain/ExternalNavigator.kt
@@ -2,4 +2,5 @@ package ru.practicum.android.diploma.vacancy.domain
 
 interface ExternalNavigator {
     fun sendEmail(email: String, subject: String)
+    fun makeCall(phone: String)
 }
--- app/src/main/java/ru/practicum/android/diploma/vacancy/domain/impl/SharingInteractorImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/vacancy/domain/impl/SharingInteractorImpl.kt
@@ -1,12 +1,19 @@
 package ru.practicum.android.diploma.vacancy.domain.impl
 
+import ru.practicum.android.diploma.common.util.Formatter
 import ru.practicum.android.diploma.vacancy.domain.ExternalNavigator
 import ru.practicum.android.diploma.vacancy.domain.SharingInteractor
+import ru.practicum.android.diploma.vacancy.domain.models.Phone
 
 class SharingInteractorImpl(
     val externalNavigator: ExternalNavigator
 ) : SharingInteractor {
     override fun sendEmail(email: String, subject: String) {
         externalNavigator.sendEmail(email, subject)
+    }
+
+    override fun makeCall(phone: Phone) {
+        val phoneString = Formatter.formatPhone(phone.country, phone.city, phone.number)
+        externalNavigator.makeCall(phoneString)
     }
 }
