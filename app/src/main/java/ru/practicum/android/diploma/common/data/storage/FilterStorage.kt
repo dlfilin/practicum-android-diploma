649146f79e180e9fe2caa4898fe4e9cc2c2c4981
Jaroslav-89 <jaroslav89@bk.ru>
2023-12-15 21:09:27 +0400

    task#157 - добавил сохранение Изменяемых настроек для фильтров

--- app/src/main/java/ru/practicum/android/diploma/common/data/storage/FilterStorage.kt
+++ app/src/main/java/ru/practicum/android/diploma/common/data/storage/FilterStorage.kt
@@ -5,5 +5,7 @@ import ru.practicum.android.diploma.common.data.storage.dto.FilterParametersDto
 interface FilterStorage {
     fun saveFilterParameters(filterParam: FilterParametersDto)
     fun getFilterParameters(): FilterParametersDto
+    fun saveEditFilterParameters(editFilterParam: FilterParametersDto)
+    fun getEditFilterParameters(): FilterParametersDto
     fun isFilterActive(): Boolean
 }
