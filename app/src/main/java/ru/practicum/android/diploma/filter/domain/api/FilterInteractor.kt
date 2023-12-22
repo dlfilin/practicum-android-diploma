e22df76446897ccaf3082751dcf482b7a10090f5
Extro21 <vostrik21@gmail.com>
2023-12-22 21:47:27 +0300

    detect

--- app/src/main/java/ru/practicum/android/diploma/filter/domain/api/FilterInteractor.kt
+++ app/src/main/java/ru/practicum/android/diploma/filter/domain/api/FilterInteractor.kt
@@ -10,7 +10,7 @@ import ru.practicum.android.diploma.filter.domain.models.Industry
 interface FilterInteractor {
     fun getIndustries(): Flow<NetworkResult<List<Industry>>>
     fun getCountries(): Flow<NetworkResult<List<Country>>>
-    fun getAreas(id : String? = null): Flow<NetworkResult<List<Area>>>
+    fun getAreas(id: String? = null): Flow<NetworkResult<List<Area>>>
     fun getCurrentFilter(): FilterParameters
     fun updateFilter(filter: FilterParameters)
 }
